2f06ce3c5db0123169c0fec87eb0fa8efd9dc883
Connor Stack <sirconnorstack@gmail.com>
2021-03-07 15:33:11 -0500

    Remove unused code

--- main.cpp
+++ main.cpp
@@ -6,13 +6,6 @@
 
 using namespace SimpleGraphicsLibrary;
 
-void processInput(Window *window);
-unsigned int VAOtriangle(float, float);
-unsigned int VAOrectangle();
-unsigned int VAOtriforce();
-unsigned int createVertexShader(const char **);
-unsigned int createShaderProgram(unsigned int, unsigned int);
-
 int main() {
   Window *window = initialize(800, 600);
   if (window == NULL) {
@@ -37,36 +30,4 @@ int main() {
 
   window->terminate();
   return 0;
-}
-
-unsigned int VAOtriforce() {
-  float vertices[] = {
-      -1.0f, -1.0f, 0.0f, // left
-      0.0f,  -1.0f, 0.0f, // right
-      -0.5f, 0.0f,  0.0f, // top
-      0.0f,  -1.0f, 0.0f, // left
-      1.0f,  -1.0f, 0.0f, // right
-      0.5f,  0.0f,  0.0f, // top
-      -0.5f, 0.0f,  0.0f, // left
-      0.5f,  0.0f,  0.0f, // right
-      0.0f,  1.0f,  0.0f  // top
-  };
-
-  unsigned int VAO, VBO;
-  glGenVertexArrays(1, &VAO);
-  glGenBuffers(1, &VBO);
-
-  glBindVertexArray(VAO);
-
-  glBindBuffer(GL_ARRAY_BUFFER, VBO);
-  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
-  glEnableVertexAttribArray(0);
-
-  glBindBuffer(GL_ARRAY_BUFFER, 0);
-
-  glBindVertexArray(0);
-
-  return VAO;
 }
