5b562cc79000646d6b56dcc03f6c23f729f71f38
Connor Stack <sirconnorstack@gmail.com>
2021-03-07 16:28:48 -0500

    Create skeleton render loop and make shapes move around.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,43 @@
 #include "simple_graphics_library.h"
 
+#include <chrono>
 #include <iostream>
 
 namespace Color = SimpleGraphicsLibrary::Color;
+
+struct state_t {
+  double box_x;
+  double box_y;
+  double triangle_x;
+  double triangle_y;
+};
+
+void render(SimpleGraphicsLibrary::Window *window, state_t *state) {
+  window->clear(Color::TEAL);
+  window->drawRectangle(state->box_x, state->box_y, 0.5, 0.5, Color::GREEN);
+  window->drawTriangle(state->triangle_x, state->triangle_y, Color::ORANGE);
+}
+
+void update(state_t *state, double dt) {
+  double new_box_x = state->box_x + 1.0f * dt;
+  if (new_box_x > 1) {
+    new_box_x -= 2;
+  }
+  state->box_x = new_box_x;
+
+  double new_triangle_y = state->triangle_y + 1.0f * dt;
+  if (new_triangle_y > 1) {
+    new_triangle_y -= 2;
+  }
+  state->triangle_y = new_triangle_y;
+}
+
+void initialize_state(state_t *state) {
+  state->box_x = 0.0;
+  state->box_y = 0.0;
+  state->triangle_x = 0.0;
+  state->triangle_y = 0.0;
+}
 
 int main() {
   SimpleGraphicsLibrary::Window *window =
@@ -12,16 +47,23 @@ int main() {
     return -1;
   }
 
+  state_t state;
+  initialize_state(&state);
+
+  std::chrono::high_resolution_clock::time_point t1 =
+      std::chrono::high_resolution_clock::now();
   while (!window->shouldClose()) {
+    std::chrono::high_resolution_clock::time_point t2 =
+        std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> dt = t2 - t1;
+    t1 = t2;
+
     if (window->keyIsPressed(GLFW_KEY_ESCAPE)) {
       window->setShouldClose(true);
     }
 
-    window->clear(Color::TEAL);
-
-    window->drawTriangle(-0.5f, 0.0f, Color::ORANGE);
-    window->drawTriangle(0.5f, 0.0f, Color::BLUE);
-    window->drawRectangle(-0.25, -0.25, 0.5, 0.5, Color::GREEN);
+    update(&state, dt.count());
+    render(window, &state);
 
     window->swapBuffers();
     window->pollForEvents();
